c101b7a12e59c42a3d9acefa5637a2688ddf6460
KMChris <km.2001@o2.pl>
2019-12-02 22:15:52 +0100

    zad 4 completed

--- 2019 01 wyszukiwanie.py
+++ 2019 01 wyszukiwanie.py
@@ -1,14 +1,62 @@
-A = [5, 99, 3, 7, 111, 13, 4, 24, 4, 8]
 
-def zad1(array):
-    left = 0
-    right = len(array)
+"""
+Małgosia i Jaś lubią liczby. Małgosia lubi liczby nieparzyste, a Jaś lubi liczby parzyste. Każde
+z dzieci zapisało po kilka spośród swoich ulubionych liczb na jednej wspólnej kartce. Najpierw
+Małgosia zapisała wszystkie swoje liczby, a potem Jaś dopisał swoje. 
+
+Napisz algorytm (w postaci listy kroków, w pseudokodzie lub w wybranym języku
+programowania), który dla danego ciągu liczb zapisanych przez dzieci znajdzie pierwszą liczbę
+zapisaną przez Jasia. Zakładamy, że każde z dzieci zapisało co najmniej jedną liczbę.
+Przy ocenie będzie brana pod uwagę złożoność czasowa Twojego algorytmu. Maksymalną
+liczbę punktów uzyskasz za algorytm o złożoności lepszej niż liniowa.
+Uwaga: W zapisie algorytmu możesz wykorzystać tylko operacje arytmetyczne (dodawanie,
+odejmowanie, mnożenie, dzielenie, dzielenie całkowite, reszta z dzielenia), instrukcje
+porównania, instrukcje sterujące i przypisania do zmiennych lub samodzielnie napisane
+funkcje, wykorzystujące wyżej wymienione operacje
+"""
+
+from random import randint
+
+
+A = []
+
+# Małgosia 
+for x in range(randint(2,8)):
+    k = randint(1,100)
+    while k % 2 == 0:
+        k = randint(1,100)
+    A.append(k)
+
+
+
+#Jaś
+for x in range(randint(2,8)):
+    k = randint(1,100)
+    while not (k % 2 == 0):
+        k = randint(1,100)
+    A.append(k)
+
+#print(A)
+
+if randint(0,1):
+    A.reverse()
+
+n = len(A)-1 #podadzą w zadaniu
+
+def zad1(A):
+    if (A[0] %2 ==0):
+        return A[0]
+
+    left = 1 
+    right = n
     while left < right:
-        mid = (left + right) // 2
-        if array[mid] % 2 != 0:
+        mid = (left+right)//2
+        if A[mid]%2 != 0:  
             left = mid + 1
-        else:
+        else:                 
             right = mid
-    return array[right]
+    return A[right]
 
+
+print(A)
 print(zad1(A))
--- 2019 04 liczby.py
+++ 2019 04 liczby.py
@@ -13,7 +13,20 @@ def zad2(numbers):
     for number in numbers:
         if sum([math.factorial(x) for x in list(map(int, [str(a) for a in str(number)]))]) == number: print(number)
 
-print('Zadanie 1:')
+def zad3(numbers):
+    first_number, length, gcd, max_length = 0, 0, 0, [0, 0, 0]
+    for i in range(1, len(numbers)):
+        first_number, gcd, length = numbers[i-1], math.gcd(numbers[i-1], numbers[i]), 1
+        while gcd>1:
+            length+=1
+            try: gcd = math.gcd(gcd, numbers[i+length-1])
+            except: break
+        if length>max_length[1]: max_length = [first_number, length, gcd]
+    print('Pierwsza liczba ciągu:', max_length[0], '\nDługość:', max_length[1], '\nNajwiększy wspólny dzielnik:', max_length[2])
+
+print('Zadanie 4.1:')
 print(zad1(liczby))
-print('\nZadanie 2:')
+print('\nZadanie 4.2:')
 zad2(liczby)
+print('\nZadanie 4.3:')
+zad3(liczby)
