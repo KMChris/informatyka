af9ccfe20eb9c00e63e08a6fdd8ed4042de9f83a
TranquiloGCC <46654827+TranquiloGCC@users.noreply.github.com>
2019-12-02 19:18:03 +0100

    Add files via upload

---
+++ 2019 01 wyszukiwanie.py
@@ -0,0 +1,17 @@
+lista = [5, 99, 3, 7, 111, 13, 4, 24, 4, 8, 12]
+
+
+def zad1(A):
+
+    left = 0
+    right = len(A)
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] % 2 != 0:
+            left = mid + 1
+        else:
+            right = mid
+    return A[right]
+
+
+print(zad1(lista))
