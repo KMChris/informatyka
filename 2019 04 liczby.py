0d067358a3f883d889c6880f9a85403117120502
KMChris <km.2001@o2.pl>
2019-12-02 21:00:08 +0100

    zad 4.2

--- 2019 04 liczby.py
+++ 2019 04 liczby.py
@@ -1,11 +1,19 @@
+import math
+
 liczby = list(map(int, open('liczby.txt', 'r').readlines()))
 
 def zad1(numbers):
     powers_of_3 = 0
     for number in numbers:
         for i in range(13):
-            if number == 3**i:
-                powers_of_3+=1
+            if number == 3**i: powers_of_3+=1
     return powers_of_3
 
+def zad2(numbers):
+    for number in numbers:
+        if sum([math.factorial(x) for x in list(map(int, [str(a) for a in str(number)]))]) == number: print(number)
+
+print('Zadanie 1:')
 print(zad1(liczby))
+print('\nZadanie 2:')
+zad2(liczby)
